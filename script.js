2524a3eb5229693e8bf048280e8a1f556372cf05
Kukusju <huj_tam@mail.ru>
2020-08-07 16:28:15 +0300

    "Otziv"

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
+
+"use strict";
 const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
-console.log(numberOfFilms);
+
+const lastFilm1 = prompt("Один из последних просмотренных фильмов?", "");
+const yourAssessment1 = prompt("На сколько оцените его?", "");
+
+const lastFilm2 = prompt("Один из последних просмотренных фильмов?", "");
+const yourAssessment2 = prompt("На сколько оцените его?", "");
 
 const personalMovieDB = {
     count: numberOfFilms, 
-    movies: "", 
+    movies: {}, 
     actors: "", 
     genres: [],
     privat: false,
 };
+
+personalMovieDB.movies[lastFilm1] = yourAssessment1;
+personalMovieDB.movies[lastFilm2] = yourAssessment2;
+
+console.log(personalMovieDB);
+
+
