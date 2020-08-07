fa2c8c369e7d44164b9f8cf782ea8be1f1ca8073
Kukusju <huj_tam@mail.ru>
2020-08-07 16:44:01 +0300

    "zadanie1"

--- script.js
+++ script.js
@@ -2,11 +2,10 @@
 "use strict";
 const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
 
-const lastFilm1 = prompt("Один из последних просмотренных фильмов?", "");
-const yourAssessment1 = prompt("На сколько оцените его?", "");
-
-const lastFilm2 = prompt("Один из последних просмотренных фильмов?", "");
-const yourAssessment2 = prompt("На сколько оцените его?", "");
+const lastFilm1 = prompt("Один из последних просмотренных фильмов?", ""),
+    yourAssessment1 = prompt("На сколько оцените его?", ""),
+    lastFilm2 = prompt("Один из последних просмотренных фильмов?", ""),
+    yourAssessment2 = prompt("На сколько оцените его?", "");
 
 const personalMovieDB = {
     count: numberOfFilms, 
